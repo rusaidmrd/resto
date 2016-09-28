d911dd545c8a34977a89b12088ddba973fe647e5
Rusaid MRD <Rusaid MRD>
2016-09-28 13:53:45 +0530

    Starting new project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Single page web application using bootstrap framework
+
+Hi every one,
+
+Another website for my coding practice, I called for this Resto based on a hotel and restaurant.
+this is a first version future will be update soon with more features.  
---
+++ js/script.js
@@ -0,0 +1,65 @@
+$(function(){
+
+	"use strict";
+	var topoffset=50;
+	var wheight=$(window).height(); // get the hieght of the window
+
+	$('.fullheight').css('height',wheight);
+
+// replace IMG inside carousel with a background image
+	$('#home .item img').each(function (){
+			var imgSrc=$(this).attr('src');
+			$(this).parent().css({ 'background-image' : 'url('+imgSrc+')' });
+			$(this).remove();
+	});
+
+// adjust height .fullheight elements on window resize
+	$(window).resize( function () {
+		wheight=$(window).height();
+		$('.fullheight').css('height',wheight);
+	});
+
+// add scroll spy
+$('body').scrollspy({
+	target:'header .navbar',
+	offset:topoffset
+});
+
+// add inbody class
+var hrefVal=$(this).find('li.active a').attr('href');
+if(hrefVal !== '#home'){
+	$('header nav').addClass('inbody');
+}else {
+	$('header nav').removeClass('inbody');
+}
+
+// Add inbody class to nav when scrollspy evet fires
+$('.navbar-fixed-top').on('activate.bs.scrollspy', function(){
+    var hrefVal=$(this).find('li.active a').attr('href');
+
+    if(hrefVal !== '#home'){
+      $('header nav').addClass('inbody');
+    }else {
+      $('header nav').removeClass('inbody');
+    }
+});
+
+
+// add smooth scrolling
+ $('.navbar a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top-topoffset+2
+        }, 1000);
+        return false;
+      }// target.length
+    }// click function
+  });// smooth scrolling
+
+
+
+// automatically generate carousel iindicators
+});
